aaee8098faf65ef88a0b8c7586e35c6c5abf6bf3
luis122448 <luis122448@gmail.com>
2023-12-18 01:47:17 +0000

    Configurations for jarfiles

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,19 @@
 # Description: Deploy the application
 
-# Create directory
-mkdir -p app/
-
-# Copy archive
-cp ./target/smart-shell-1.0.0.jar ./app/smart-shell.jar
-
 # Stop the application
 sudo docker-compose down
 
 # Pull the latest changes
 sudo git pull origin main
+
+# Create directory
+mkdir -p app/
+
+# Delete content
+rm -rf app/*
+
+# Copy archive
+cp ./target/smart-shell-1.0.0.jar ./app/smart-shell.jar
 
 # Build and run the application
 sudo docker-compose up --build --force-recreate --no-deps -d
