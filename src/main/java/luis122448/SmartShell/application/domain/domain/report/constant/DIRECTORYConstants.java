28beb2194289706d5ebb646aaf03957957c1f1bb
luis122448 <luis122448@gmail.com>
2024-02-18 22:20:15 -0500

    Update DIRECTORYConstants

--- src/main/java/luis122448/SmartShell/application/domain/domain/report/constant/DIRECTORYConstants.java
+++ src/main/java/luis122448/SmartShell/application/domain/domain/report/constant/DIRECTORYConstants.java
@@ -1,11 +1,14 @@
 package luis122448.SmartShell.application.domain.domain.report.constant;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class DIRECTORYConstants {
 
-    public static String projectRoot = System.getProperty("user.dir");
-    public static final String REPORT_INVOICE_A4_HORIZONTAL = projectRoot + "/src/main/java/luis122448/SmartShell/application/domain/domain/report/archive/invoce-a4-horizontal.jasper";
-    public static final String REPORT_ERROR_IMPORT_A4_HORIZONTAL = projectRoot + "/src/main/java/luis122448/SmartShell/application/domain/domain/report/archive/error-import-a4-horizontal.jasper";
-    public static final String REPORT_SUCCESS_IMPORT_A4_HORIZONTAL = projectRoot + "/src/main/java/luis122448/SmartShell/application/domain/domain/report/archive/success-import-a4-horizontal.jasper";
-    public static final String FORMAT_PRICE_LIST_ARTICLE = projectRoot + "/src/main/java/luis122448/SmartShell/application/domain/domain/report/archive/list-price-article.xlsx";
-    public static final String FORMAT_ARTICLE = projectRoot + "/src/main/java/luis122448/SmartShell/application/domain/domain/report/archive/format-article.xlsx";
+    public static Path projectRoot = Paths.get(System.getProperty("user.dir"));
+    public static final String REPORT_INVOICE_A4_HORIZONTAL = projectRoot + "/src/main/resources/archive/jasper/invoice-a4-horizontal.jasper";
+    public static final String REPORT_ERROR_IMPORT_A4_HORIZONTAL = projectRoot + "/src/main/resources/archive/jasper/error-import-a4-horizontal.jasper";
+    public static final String REPORT_SUCCESS_IMPORT_A4_HORIZONTAL = projectRoot + "/src/main/resources/archive/jasper/success-import-a4-horizontal.jasper";
+    public static final String FORMAT_PRICE_LIST_ARTICLE = projectRoot + "/src/main/resources/archive/excel/list-price-article.xlsx";
+    public static final String FORMAT_ARTICLE = projectRoot + "/src/main/resources/archive/excel/format-article.xlsx";
 }
