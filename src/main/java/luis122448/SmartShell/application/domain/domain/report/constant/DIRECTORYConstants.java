0fee34f3933b118f5b4ee9b988d105b4a3a4b532
luis122448 <luis122448@gmail.com>
2024-05-15 23:21:05 -0500

    Update DIRECTORYConstants again

--- src/main/java/luis122448/SmartShell/application/domain/domain/report/constant/DIRECTORYConstants.java
+++ src/main/java/luis122448/SmartShell/application/domain/domain/report/constant/DIRECTORYConstants.java
@@ -6,15 +6,16 @@ import java.nio.file.Paths;
 public class DIRECTORYConstants {
 
     public static Path projectRoot = Paths.get(System.getProperty("user.dir")).toAbsolutePath();
-    public static final String REPORT_DIRECTORY = projectRoot + "src/main/resources/archive/jasper/";
-    public static final String EXCEL_DIRECTORY = projectRoot + "src/main/resources/archive/excel/";
+    public static final Path REPORT_DIRECTORY = projectRoot.resolve("src/main/resources/archive/jasper/");
+    public static final Path EXCEL_DIRECTORY = projectRoot.resolve("src/main/resources/archive/excel/");
 
-    public static final String REPORT_INVOICE_A4_HORIZONTAL = REPORT_DIRECTORY + "invoice-a4-horizontal.jasper";
-    public static final String REPORT_INVOICE_A4_VERTICAL = REPORT_DIRECTORY + "invoice-a4-vertical.jasper";
-    public static final String REPORT_RECEIPT_A4_HORIZONTAL = REPORT_DIRECTORY + "receipt-a4-horizontal.jasper";
-    public static final String REPORT_RECEIPT_A4_VERTICAL = REPORT_DIRECTORY + "receipt-a4-vertical.jasper";
-    public static final String REPORT_ERROR_IMPORT_A4_HORIZONTAL = REPORT_DIRECTORY + "error-import-a4-horizontal.jasper";
-    public static final String REPORT_SUCCESS_IMPORT_A4_HORIZONTAL = REPORT_DIRECTORY + "success-import-a4-horizontal.jasper";
-    public static final String FORMAT_PRICE_LIST_ARTICLE = EXCEL_DIRECTORY + "list-price-article.xlsx";
-    public static final String FORMAT_ARTICLE = EXCEL_DIRECTORY + "format-article.xlsx";
+    public static final String REPORT_INVOICE_A4_HORIZONTAL = REPORT_DIRECTORY.resolve("invoice-a4-horizontal.jasper").toString();
+    public static final String REPORT_INVOICE_A4_VERTICAL = REPORT_DIRECTORY.resolve("invoice-a4-vertical.jasper").toString();
+    public static final String REPORT_RECEIPT_A4_HORIZONTAL = REPORT_DIRECTORY.resolve("receipt-a4-horizontal.jasper").toString();
+    public static final String REPORT_RECEIPT_A4_VERTICAL = REPORT_DIRECTORY.resolve("receipt-a4-vertical.jasper").toString();
+    public static final String REPORT_ERROR_IMPORT_A4_HORIZONTAL = REPORT_DIRECTORY.resolve("error-import-a4-horizontal.jasper").toString();
+    public static final String REPORT_SUCCESS_IMPORT_A4_HORIZONTAL = REPORT_DIRECTORY.resolve("success-import-a4-horizontal.jasper").toString();
+    public static final String FORMAT_PRICE_LIST_ARTICLE = EXCEL_DIRECTORY.resolve("list-price-article.xlsx").toString();
+    public static final String FORMAT_ARTICLE = EXCEL_DIRECTORY.resolve("format-article.xlsx").toString();
+
 }
