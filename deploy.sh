ffab22f687dd2c4ca73f3c627d779f5eb0529e38
luis122448 <luis122448@gamil.com>
2024-07-20 07:54:21 -0500

    chore(deploy) update deploy and docker-compose

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 # Description: Deploy the application
 
 # Stop the application
@@ -6,21 +5,6 @@ sudo docker compose down
 
 # Pull the latest changes
 sudo git pull origin main
-
-# Create directory
-mkdir -p app/
-
-# Delete content
-rm -rf app/*
-
-# Copy archive
-cp ./target/smart-shell-1.0.0.jar ./app/smart-shell.jar
-
-#
-chmod +x ./dev-install.sh
-
-# Install scrip
-sudo bash ./dev-install.sh
 
 # Build and run the application
 sudo docker compose up --build --force-recreate --no-deps -d
