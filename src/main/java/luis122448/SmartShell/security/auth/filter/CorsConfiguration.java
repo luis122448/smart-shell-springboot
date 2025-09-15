75e8e0760d488a158d8da4fb80fb873f4a6e9830
luis122448 <luis122448@gmail.com>
2025-09-14 20:08:46 -0500

    feat: add CORS_ALLOWED_ORIGINS environment

--- src/main/java/luis122448/SmartShell/security/auth/filter/CorsConfiguration.java
+++ src/main/java/luis122448/SmartShell/security/auth/filter/CorsConfiguration.java
@@ -1,5 +1,6 @@
 package luis122448.SmartShell.security.auth.filter;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,24 +9,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfiguration {
 
+    @Value("${cors.allowed-origins}")
+    private String[] allowedOrigins;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        // .allowedOrigins(
-                        //         "http://localhost:*",
-                        //         "http://smart-shell-angular:*",
-                        //         "https://smart-shell.luis122448.com",
-                        //         "http://smart-shell.luis122448.com"
-                        // )
-                        .allowedOriginPatterns(
-                                "http://localhost:*",
-                                "http://smart-shell-angular:*",
-                                "https://smart-shell.luis122448.com",
-                                "http://smart-shell.luis122448.com"
-                        )
+                        .allowedOriginPatterns(allowedOrigins)
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true)
